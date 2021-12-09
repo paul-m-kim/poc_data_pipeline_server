3c566590fc51ffea018d44f72fd7834738b14215
Paul Kim <muhyun.paul.kim@gmail.com>
2021-12-09 09:32:42 -0800

    Initial commit: setup and planning

---
+++ src/client.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char **argv)
+{
+
+    printf("hello World\n");
+
+    return 0;
+
+}
---
+++ src/server.c
@@ -0,0 +1,44 @@
+/**
+ * General:
+ * Testing
+ * Debug mode
+ * logs?
+ * Code guidelines
+ *   variable format
+ *   function format
+ * 
+ */
+
+/**
+ * Data Pipeline Server Components:
+ * n x Data Connector
+ * Data Writer (manages data connectors)
+ *   Extraction Transformation? and Loading (ETL)
+ *   Transformation Examples: parser, joiner, convert (csv, json)
+ * Message Queue
+ * Data Producer (produces data to client)
+ * Request Processor (processes requests from client)
+ * 
+ */
+
+/**
+ * External Data Source Server Components
+ * 
+ */
+
+/**
+ * Client Components
+ * 
+ */
+
+/**
+ * API for Client
+ * - add data sources (REST/TCP Socket/WebSocket[hard? realtime])
+ *   + batch data (duration?)
+ *   + real-time data
+ * - query data (Data Lake / Data Warehouse)
+ *   + text (csv?, json?)
+ *   + images
+ *   + videos
+ * 
+ */
